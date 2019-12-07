33ddd1c49022a91b4156f38bc06fa547bf9d99b5
Camila Rivera <camila.riverart@gmail.com>
2019-12-06 16:58:49 -0800

    setup input component and and store factory in testutils

---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+ function Input() {
+    return <div>
+        <button></button>
+    </div>;
+};
+
+const mapStateToProps = (state) => {
+    return {};
+}
+
+export default connect(mapStateToProps)(Input);
